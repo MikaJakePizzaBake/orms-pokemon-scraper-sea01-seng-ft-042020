87fded14fe054b604a3bb2f472682cfffadf3aed
Trevor Low <tlow94@uw.edu>
2020-05-01 04:16:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,4 +10,8 @@ class Pokemon
 def self.save(name, type, db)
   db.execute("INSERT INTO pokemon(name, type)VALUES(?,?);", name, type)
 end
+def self.find
+  db.execute("SELECT*FROM pokemon WHERE id = ?";, id).flatten
+  
+end
 end
